c81c84099e29716067c62e16c8196750394a4cd2
DXT0509 <thaodao05092005@gmail.com>
2026-04-06 15:30:36 +0700

    add all-uses testing

---
+++ src/test/java/AllUses.java
@@ -0,0 +1,96 @@
+import application.Person;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class AllUses {
+
+  public double CalculateTips(int time, int age) {
+    if ( time <= 8 || time >= 23 || age <= 0 || age > 100 ) {
+      return -1;
+    }
+    else if ( time >= 9 && time <= 16) {
+      double x = 149;
+      if ( age < 6 ) return 0;
+      else if ( age >= 6 && age <= 10 ) return x*0.5;
+      else if ( age >= 11 && age <= 60 ) return x;
+      else return x*0.6-20;
+    }
+    else {
+      double x = 199;
+      if ( age < 6 ) return 0;
+      else if ( age >= 6 && age <= 10 ) return x*0.7;
+      else if ( age >= 11 && age <= 60 ) return x;
+      else return x*0.6;
+    }
+  }
+
+  @Test
+  void test1() {
+    double result = CalculateTips(7,20);
+
+    Assertions.assertEquals(-1, result, 0.001);
+  }
+  @Test
+  void test2() {
+    double result = CalculateTips(10,4);
+
+    Assertions.assertEquals(0, result, 0.001);
+  }
+
+  @Test
+  void test3() {
+    double result = CalculateTips(20,4);
+
+    Assertions.assertEquals(0, result, 0.001);
+  }
+
+  @Test
+  void test4() {
+    double result = CalculateTips(16,-2);
+
+    Assertions.assertEquals(-1, result, 0.001);
+  }
+
+  @Test
+  void test5() {
+    double result = CalculateTips(10,8);
+
+    Assertions.assertEquals(74.5, result, 0.001);
+  }
+
+  @Test
+  void test6() {
+    double result = CalculateTips(10,25);
+
+    Assertions.assertEquals(149, result, 0.001);
+  }
+
+  @Test
+  void test7() {
+    double result = CalculateTips(10,70);
+
+    Assertions.assertEquals(69.4, result, 0.001);
+  }
+
+  @Test
+  void test8() {
+    double result = CalculateTips(20,8);
+
+    Assertions.assertEquals(139.3, result, 0.001);
+  }
+
+  @Test
+  void test9() {
+    double result = CalculateTips(20,25);
+
+    Assertions.assertEquals(199, result, 0.001);
+  }
+
+  @Test
+  void test10() {
+    double result = CalculateTips(20,70);
+
+    Assertions.assertEquals(119.4, result, 0.001);
+  }
+}
+
